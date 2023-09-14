e79e3e0d575077c1894ace51d0da5b030b7f03e4
eliasameh100 <amehelias112@gmail.com>
2023-09-14 00:15:13 -0700

    made updates

--- processes/update.php
+++ processes/update.php
@@ -3,6 +3,17 @@ session_start();
 include "../classes/Cart.php";
 if(isset($_GET['id'])){
     $id=$_GET['id'];
-    $itemprice=$_SESSION
+    $itemprice=$_SESSION['itemprice'];
+    $cart_Instance=new Carts();
+    $result=$cart_Instance->increaseQuantity($itemprice,$id);
+    if(!$result){
+        $_SESSION['error']="Something Is Wrong";
+        header("location:../cart.php");
+        exit();
+    }else{
+        $_SESSION['success']="Updated Successfully"
+        header("location:../cart.php")
+        exit();
+    }
 }
 ?>
