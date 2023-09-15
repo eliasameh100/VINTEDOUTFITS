aa86ba2d5bcac191b8e92590d96c675aefb7fbdd
eliasameh100 <amehelias112@gmail.com>
2023-09-14 20:43:23 -0700

    made index page good

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome page</title>
+    <title>Index</title>
     <link rel="stylesheet" href="assets/bootstrap-5.0.2-dist/css/bootstrap.rtl.min.css">
 </head>
 <style>
@@ -18,14 +18,73 @@
     background-repeat: no-repeat;
     background-size: center;
 }
+    img{
+    width: 15rem;
+    height: 9rem;
+    }
 </style>
     <?php include_once "components/navbar.php";?>
     <div class="container-fluid">
         <div class="content">
-            <p class="h2 text-center mb-2">VINTEDOUTFITS</p>
-            <p class="h6 text-center mt-2">Motto:: Giving You The Best Of Looks</p>
-            <h3 class="mt-2">Welcome Extinguish User</h3>
+            <p class="h1 text-center mb-2">VINTEDOUTFITS</p>
+            <p class="h6 mt-2 text-center">Motto:: Giving You The Best Of Looks</p>
+            <h3 class="text-center my-3">Welcome Extinguish User</h3>
         </div>
-
+        <div class="my-2 container text-center">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, omnis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, non.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, sit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, vitae.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, porro.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, rem.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, facere.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, voluptatum.
+        </div>
+        <center>
+            <div class="container ">
+                <img class="" src="assets/images/suit.png" alt="">
+                <img class="" src="assets/images/clothes2.jpg" alt="">
+                <img class="" src="assets/images/clothes.jpg" alt="">
+            </div>
+        </center>
+        <div class="my-2 container text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, incidunt.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, sit.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, nihil.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, exercitationem?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, corporis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, possimus!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, necessitatibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequuntur.
+        </div>
+        <center>
+            <div class="container ">
+                <img class="" src="assets/images/Asym.png" alt="">
+                <img class="" src="assets/images/bermuda.png" alt="">
+                <img class="" src="assets/images/slip.jpg" alt="">
+            </div>
+        </center>
+        <div class="my-2 container text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, incidunt.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, sit.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, nihil.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, exercitationem?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, corporis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, possimus!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, necessitatibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequuntur.
+        </div>
+        <center>
+            <div class="container ">
+                <img class="" src="assets/images/download.jpg" alt="">
+                <img class="" src="assets/images/images (1).jpg" alt="">
+                <img class="" src="assets/images/images (3).jpg" alt="">
+            </div>
+        </center>
+        <center>
+            <div class="my-2">
+                <a class="btn btn-info text-center" href="index.php#">Top^</a>
+            </div>
+        </center>
 </body>
 </html>
